5c0c045fc42af6ac630809a44f6c1c8e39c4bdbb
Adam777 <Adam777-5867@outlook.com>
2018-12-27 13:22:20 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  var unit = 'C';
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $.getJSON('https://fcc-weather-api.glitch.me/api/current?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude, function(data) {
+        $('#location').html(data.name + ', ' + data.sys.country);
+        $("#temperature").html(Math.round(parseInt(data.main.temp, 10))).after(' °');
+        $("#unit").html(unit);
+        $('#description').html('<img src="' + data.weather[0].icon + '">' + data.weather[0].main);
+      });
+    });
+  }
+  
+  $('#unit').click(function() {
+    var temperature = $('#temperature').text();
+    
+    if (unit == 'C') {
+      unit = 'F';
+      $('#temperature').html(Math.round(parseInt(temperature, 10)*(9/5)+32));
+      $('#unit').html(unit);
+    } else if (unit == 'F') {
+      unit = 'C';
+      $('#temperature').html(Math.round((parseInt(temperature, 10)-32)*(5/9)));
+      $('#unit').html(unit);
+    }
+  });
+});
